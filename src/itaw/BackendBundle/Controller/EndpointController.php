4b89119229215e4de59fb432f83e23ebf047ae66
Weber, Florian <fweber@ligneus.de>
2015-01-05 15:18:35 +0100

    collection, object, create

--- src/itaw/BackendBundle/Controller/EndpointController.php
+++ src/itaw/BackendBundle/Controller/EndpointController.php
@@ -3,11 +3,72 @@
 namespace itaw\BackendBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use itaw\DataBundle\Entity\Endpoint;
+use itaw\Helper\EndpointHelper;
 
 /**
  * @author Florian Weber <fweber@ligneus.de>
  */
 class EndpointController extends Controller
 {
-    
+    public function collectionAction()
+    {
+        $endpoints = $this->getDoctrine()->getRepository('itawDataBundle:Endpoint')->findAll();
+
+        return $this->render('itawBackendBundle:Endpoint:collection.html.twig', array(
+                    'endpoints' => $endpoints,
+        ));
+    }
+
+    public function objectAction($slug)
+    {
+        $endpoint = $this->getDoctrine()->getRepository('itawDataBundle:Endpoint')->findOneBySlug($slug);
+
+        return $this->render('itawBackendBundle:Endpoint:object.html.twig', array(
+                    'endpoint' => $endpoint,
+        ));
+    }
+
+    public function createAction(Request $request)
+    {
+        $session = $request->getSession();
+
+        if ($request->get('sent', 0) == 1) {
+            $endpoint = new Endpoint();
+            $endpoint->setHost($request->get('host'))
+                    ->setIp($request->get('ip'))
+                    ->setName($request->get('name'))
+                    ->setSlug(EndpointHelper::generateSlug($request->get('host').$request->get('name')))
+            ;
+
+            //validate
+            $validator = $this->get('validator');
+            $errors = $validator->validate($endpoint);
+
+            if (count($errors) > 0) {
+                foreach ($errors as $error) {
+                    $session->getFlashBag()->add('error', $error);
+                }
+
+                return $this->render('itawBackendBundle:Endpoint:create.html.twig');
+            }
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($endpoint);
+            $em->flush();
+
+            return $this->redirectToRoute('backend_endpoints_collection');
+        }
+
+        return $this->render('itawBackendBundle:Endpoint:create.html.twig');
+    }
+
+    public function updateAction()
+    {
+    }
+
+    public function deleteAction()
+    {
+    }
 }
