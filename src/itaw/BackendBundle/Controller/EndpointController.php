e12532e8e320608ee9e121e24caa33282c6e9054
Weber, Florian <fweber@ligneus.de>
2015-01-05 14:56:51 +0100

    endpoint controller

---
+++ src/itaw/BackendBundle/Controller/EndpointController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace itaw\BackendBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * @author Florian Weber <fweber@ligneus.de>
+ */
+class EndpointController extends Controller
+{
+    
+}
