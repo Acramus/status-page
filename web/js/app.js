b2da3d43308cc873474f4bb6e60ba927f47901e6
Weber, Florian <fweber@ligneus.de>
2015-01-05 14:16:10 +0100

    base assets

---
+++ web/js/app.js
@@ -0,0 +1,11 @@
+/**
+ * ITAW Status Page
+ * 
+ * app.js
+ * 
+ * @author Florian Weber <florian.weber.dd@icloud.com>
+ */
+
+$(document).ready(function () {
+
+});
---
+++ web/js/status.js
@@ -0,0 +1,11 @@
+/**
+ * ITAW Status Page
+ * 
+ * status.js
+ * 
+ * @author Florian Weber <florian.weber.dd@icloud.com>
+ */
+
+$(document).ready(function () {
+
+});
