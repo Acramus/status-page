b1e8921c73be6af3b139be75fdf87fd70a9c8b2d
Weber, Florian <fweber@ligneus.de>
2015-01-06 07:08:38 +0100

    validation

--- src/itaw/DataBundle/Entity/Endpoint.php
+++ src/itaw/DataBundle/Entity/Endpoint.php
@@ -32,7 +32,7 @@ class Endpoint
     /**
      * @var string
      *
-     * @ORM\Column(name="slug", type="string", length=255)
+     * @ORM\Column(name="slug", type="string", length=255, unique=true)
      */
     private $slug;
 
